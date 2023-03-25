52bf27e76dcd2ea5ecdfa7af82e387da33e65f26
silkrow <si1krow@outlook.com>
2023-03-25 01:53:38 -0500

    starting

---
+++ chess-opening/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+
+import { Chess } from 'chess.js'
+
+const chess = new Chess()
+
+while (!chess.isGameOver()) {
+  const moves = chess.moves()
+  const move = moves[Math.floor(Math.random() * moves.length)]
+  chess.move(move)
+}
+console.log(chess.pgn())
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
